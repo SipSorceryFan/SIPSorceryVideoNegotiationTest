fb63610c210071f849a0d5ebdef8c2223887ee1a
SipSorceryFan <sipsorceryfan@protonmail.com>
2023-08-23 19:04:52 -0400

    SIPSorcery format negotiation test project

---
+++ SIPSorceryVideoNegotiationTest/MultiFormatVideoSource.cs
@@ -0,0 +1,62 @@
+﻿using SIPSorceryMedia.Abstractions;
+
+namespace SIPSorceryVideoNegotiationTest
+{
+    public class MultiFormatVideoSource : IVideoSource
+    {
+        private const int H264_FORMAT_ID = 100;
+        private const int VP8_FORMAT_ID = 96;
+        private static readonly List<VideoFormat> SupportedVideoFormats = new List<VideoFormat>
+        {
+            new VideoFormat(VideoCodecsEnum.VP8, VP8_FORMAT_ID, VideoFormat.DEFAULT_CLOCK_RATE),
+            new VideoFormat(VideoCodecsEnum.H264, H264_FORMAT_ID, VideoFormat.DEFAULT_CLOCK_RATE)
+        };
+
+        public event EncodedSampleDelegate OnVideoSourceEncodedSample;
+
+        public event SourceErrorDelegate OnVideoSourceError;
+
+        public event RawVideoSampleDelegate OnVideoSourceRawSample;
+
+        public event RawVideoSampleFasterDelegate OnVideoSourceRawSampleFaster;
+
+        public Task CloseVideo() => Task.CompletedTask;
+
+        public void ExternalVideoSourceRawSample(uint durationMilliseconds, int width, int height, byte[] sample, VideoPixelFormatsEnum pixelFormat)
+        {
+            //
+        }
+
+        public void ExternalVideoSourceRawSampleFaster(uint durationMilliseconds, RawImage rawImage)
+        {
+            //
+        }
+
+        public void ForceKeyFrame()
+        {
+            //
+        }
+
+        public List<VideoFormat> GetVideoSourceFormats() => SupportedVideoFormats;
+
+        public bool HasEncodedVideoSubscribers() => false;
+
+        public bool IsVideoSourcePaused() => true;
+
+        public Task PauseVideo() => Task.CompletedTask;
+
+        public void RestrictFormats(Func<VideoFormat, bool> filter)
+        {
+            //
+        }
+
+        public Task ResumeVideo() => Task.CompletedTask;
+
+        public void SetVideoSourceFormat(VideoFormat videoFormat)
+        {
+            Console.WriteLine($"{nameof(MultiFormatVideoSource)} Video Source Format Id: {videoFormat.FormatID}");
+        }
+
+        public Task StartVideo() => Task.CompletedTask;
+    }
+}
---
+++ SIPSorceryVideoNegotiationTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using SIPSorcery.Media;
+using SIPSorcery.SIP;
+using SIPSorcery.SIP.App;
+using SIPSorceryMedia.Abstractions;
+
+namespace SIPSorceryVideoNegotiationTest
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var recipientSipTransport = new SIPTransport();
+
+            recipientSipTransport.AddSIPChannel(new SIPUDPChannel(IPAddress.Any, 5556));
+            recipientSipTransport.AddSIPChannel(new SIPUDPChannel(IPAddress.IPv6Any, 5556));
+            recipientSipTransport.AddSIPChannel(new SIPTCPChannel(IPAddress.Any, 5556));
+
+            var recipientUserAgent = new SIPUserAgent(recipientSipTransport, null, true);
+
+            recipientUserAgent.OnIncomingCall += (agent, request) =>
+            {
+                var serverUserAgent = agent.AcceptCall(request);
+
+                var singleFormatVideoSourceMediaEndPoints = new MediaEndPoints
+                {
+                    VideoSource = new SingleFormatVideoSource()
+                };
+
+                _ = agent.Answer(serverUserAgent, new VoIPMediaSession(singleFormatVideoSourceMediaEndPoints));
+            };
+
+            var callerSipTransport = new SIPTransport();
+
+            callerSipTransport.AddSIPChannel(new SIPUDPChannel(IPAddress.Any, 5555));
+            callerSipTransport.AddSIPChannel(new SIPUDPChannel(IPAddress.IPv6Any, 5555));
+            callerSipTransport.AddSIPChannel(new SIPTCPChannel(IPAddress.Any, 5555));
+
+            var callerUserAgent = new SIPUserAgent(callerSipTransport, null, true);
+
+            var multiFormatVideoSourceMediaEndPoints = new MediaEndPoints
+            {
+                VideoSource = new MultiFormatVideoSource()
+            };
+
+            _ = await callerUserAgent.Call("sip:127.0.0.1:5556", null, null, new VoIPMediaSession(multiFormatVideoSourceMediaEndPoints));
+
+            Console.ReadLine();
+        }
+    }
+}
